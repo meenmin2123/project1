1394ced1a00b624e80fbcf6a5bc9cc1504559dc2
meenmin2123 <80881753+meenmin2123@users.noreply.github.com>
2024-10-23 17:23:52 +0900

    24.10.23

--- elastic/src/main/java/com/ss/elastic/controller/ElasticRestController.java
+++ elastic/src/main/java/com/ss/elastic/controller/ElasticRestController.java
@@ -1,11 +1,19 @@
 package com.ss.elastic.controller;
 
+import co.elastic.clients.elasticsearch.core.BulkRequest;
+import co.elastic.clients.elasticsearch.core.BulkResponse;
+import org.springframework.web.client.RestTemplate;
 import com.ss.elastic.service.ElasticService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestOperations;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/elastic")
@@ -22,14 +30,63 @@ public class ElasticRestController {
             // 인덱스 생성을 위해서 매개변수로 데이터를 넘김.
             service.createIndex(indexName);
             return "인덱스 생성 성공!" + indexName;
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             return "인덱스 생성 실패!" + indexName;
         }
-
-
     }
 
+    // http://localhost:8080/elastic/create?indexName=my_index
+    // my_index에 도큐먼트 저장하는 메서드
+    // ex) PUT my_index/_doc/1
+    @PostMapping("/add/{indexName}/{docId}")
+    public String InsertDocument(@PathVariable String indexName, @PathVariable String docId) throws IOException {
+        System.out.println("InsertDocument() 실행");
+        System.out.println("docId : " + docId);
+
+        // 문서 데이터 랜덤 생성
+        Map<String, Object> docDataMap = randomDocument();
+
+        try {
+            return service.InsertDocument(indexName, docId, docDataMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @GetMapping("/get/{indexName}/{docId}")
+    public Map<String, Object> getDocument(@PathVariable String indexName, @PathVariable String docId) {
+        System.out.println("getDocument()");
+
+        Map<String, Object> docMap = service.getDocument(indexName, docId);
+        System.out.println(docMap.toString());
+
+        return docMap;
+    }
+
+    // 학생의 데이터를 한 번에 저장하는 메서드!
+    public String bulkAddDocument(String indexName, List<Map<String, Object>> stu) {
+
+        return "";
+    }
+
+
+
+
+    private Map<String, Object> randomDocument() {
+
+        Random randomId = new Random();
+        Map<String, Object> docData = new HashMap<String, Object>();
+
+        docData.put("name", "user" + randomId.nextInt(100));
+        docData.put("age", randomId.nextInt(40) + 20);
+        docData.put("gender", randomId.nextBoolean() ? "male" : "female");
+
+        System.out.println("docData : " + docData);
+
+        return docData;
+    }
 
 }
--- elastic/src/main/java/com/ss/elastic/service/ElasticService.java
+++ elastic/src/main/java/com/ss/elastic/service/ElasticService.java
@@ -1,12 +1,19 @@
 package com.ss.elastic.service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.*;
+import co.elastic.clients.elasticsearch.core.bulk.BulkOperation;
+import co.elastic.clients.elasticsearch.core.bulk.IndexOperation;
 import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
 import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.List;
 
 @Service
 public class ElasticService {
@@ -15,6 +22,12 @@ public class ElasticService {
     // 엘라스틱의 정보를 가지고 있는 엘라스틱 클라이언트 객체가 필요함.
     @Autowired
     private ElasticsearchClient elasticsearchClient;
+
+    private final RestTemplate restTemplate;
+
+    public ElasticService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     // 인덱스를 생성하는 메서드'
     public void createIndex(String indexName) throws IOException {
@@ -40,5 +53,91 @@ public class ElasticService {
             System.out.println("Index " + indexName + " failed");
         };
 
+    }
+
+    // 한 개의 데이터만 저장하는 메서드(한 개 도큐먼트)
+    public String InsertDocument(String indexName, String docId, Map<String, Object> docDataMap) throws IOException {
+        System.out.println("ElasticService-InsertDocument()");
+
+        // 문서를 엘라스틱에 추가하기 위해서 요청을 만듦.
+        IndexRequest<Map<String, Object>> indexRequest = new IndexRequest.Builder<Map<String, Object>>()
+                                                                         .index(indexName)   // 인덱스 이름 설정
+                                                                         .index(docId)       // 문서 ID 설정
+                                                                         .document(docDataMap)
+                                                                         .build();
+        // 엘라스틱 서치로 전송(restclient)하는 메서드
+        IndexResponse resp = elasticsearchClient.index(indexRequest);
+
+        // 결과 확인
+        // - put 요청이 들어가게 되면 데이터가 없음 -> 데이터 생성, 데이터 있으면 -> 업데이트
+        System.out.println("ElasticService-InsertDocument(): " + resp.toString());
+        System.out.println(resp.result());
+        return "문서가 성공적으로 추가되었음. ";
+    }
+
+    // 문서를 조회하는 메서드
+    public Map<String, Object> getDocument(String indexName, String docId) {
+
+        // 문서 조회 요청 객체 생성
+        try {
+            GetRequest request = new GetRequest.Builder().index(indexName).id(docId).build();
+
+            // 엘라스틱에 전송
+            // 첫번째 매개변수는 요청 객체 작성
+            // 두번째 매개변수는 결과를 어떤 타입으로 받을지 설정
+            GetResponse<Map> resp = elasticsearchClient.get(request, Map.class);
+
+            if(resp.found()) {
+                return resp.source();
+            } else {
+                System.out.println("ElasticService-GetDocument(): " + resp.toString());
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // 학생의 데이터를 한 번에 저장하는 메서드!
+    public String bulkAddDocument(String indexName, List<Map<String, Object>> stu) throws Exception{
+
+        // 1. 요청하는 객체를 빌더생성!
+        BulkRequest.Builder bulkRequestBuilder = new BulkRequest.Builder();
+
+        // 각 학생 데이터를 반복문으로 처리
+        for(Map<String, Object> st : stu) {
+            IndexOperation<Map<String, Object>> op = new IndexOperation
+                                                        .Builder<Map<String, Object>>()
+                                                        .index(indexName)  //인덱스 이름 설정
+                                                        .document(st)      //학생 데이터 저장
+                                                        .build();
+            // 요청에 위에 문서를 추가
+            BulkOperation bulkOperation = new BulkOperation.Builder().index(op).build();
+            bulkRequestBuilder.operations(bulkOperation);
+        }
+        //전송
+        // bulk() 메서드가 전송해서 처리한다.
+        BulkRequest bulkRequest = bulkRequestBuilder.build();
+        BulkResponse bulkResp  = elasticsearchClient
+                .bulk(bulkRequest);
+
+        System.out.println(bulkResp.toString());
+        System.out.println(bulkResp.errors());
+        // 2. 결과를 리턴하기!
+        if(!bulkResp.errors()) {
+            return "정상적으로 저장 되었습니다.";
+        }else {
+            return "저장 실패!!!";
+        }
+
+    }
+
+    public String searchIndex() {
+        String url = "http://localhost:9200/hee_index/_search?pretty";
+
+        ResponseEntity<String> resp = restTemplate.getForEntity(url, String.class);
+
+        return resp.getBody();
     }
 }
